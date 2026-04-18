60199a67dbde04101395977141b9102583db99d1
Joshua <joshuapogi123123123@gmail.com>
2026-04-18 13:30:12 +0800

    declare player score variables and create a function playround and its logic

--- script.js
+++ script.js
@@ -15,6 +15,33 @@ function getHumanChoice () {
   return humanChoice.toLowerCase();
 }
 
+  let computerScore = 0;
+  let humanScore = 0;
+
+function playRound (humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    return "It's a tie!";
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    humanScore++;
+    return "You win! Rock beats scissors.";
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    humanScore++;
+    return "You win! Paper beats rock.";
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    humanScore++;
+    return "You win! Scissors beats paper.";
+  } else {
+    computerScore++;
+    return 'You Lose Computer Wins'
+    
+  }
+
+  }
+
+
+
+
+
 
 
 
