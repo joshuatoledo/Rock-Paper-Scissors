0a67884ab4f5860955ae5a656661e92e596e453f
Joshua <joshuapogi123123123@gmail.com>
2026-04-18 13:26:51 +0800

    created a function getHumanChoice and its logic

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+function getComputerChoice () {
+ const randomNumber = Math.random();
+  if (randomNumber < 0.33) {
+   return "rock";
+ } else if (randomNumber < 0.66) {
+   return "paper";
+ } else {
+   return "scissors";
+ }
+
+}
+
+function getHumanChoice () {
+  let humanChoice = prompt("Please choose rock, paper, or scissors.");
+  return humanChoice.toLowerCase();
+}
+
+
+
+
+
