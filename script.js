c9639821c11e0e5082fdd218aaae61fb99451712
Joshua Toledo <joshuapogi123123123@gmail.com>
2026-05-17 21:32:46 +0800

    refactor the old rock paper scissors game

--- script.js
+++ script.js
@@ -10,61 +10,76 @@ function getComputerChoice () {
 
 }
 
-function getHumanChoice () {
-  let humanChoice = prompt("Please choose rock, paper, or scissors.");
-  return humanChoice.toLowerCase();
+function hasPlayerWonTheRound(player, computer) {
+  return (
+    (player === "rock" && computer === "scissors") ||
+    (player === "scissors" && computer === "paper") ||
+    (player === "paper" && computer === "rock")
+  );
 }
-
- 
-function playGame () {
 
  let computerScore = 0;
-  let humanScore = 0;
- 
+ let humanScore = 0;
 
-function playRound (humanChoice, computerChoice) {
-   let result = "";
-  if (humanChoice === computerChoice) {
-    result =  "It's a tie!";
-  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+ function getRoundResults(userOption) {
+  const computerResult = getComputerChoice();
+
+  if (hasPlayerWonTheRound(userOption, computerResult)) {
     humanScore++;
-    result =  "You win! Rock beats scissors.";
-  } else if (humanChoice === "paper" && computerChoice === "rock") {
-    humanScore++;
-     result = "You win! Paper beats rock.";
-  } else if (humanChoice === "scissors" && computerChoice === "paper") {
-    humanScore++;
-    result =  "You win! Scissors beats paper.";
+    return `Player wins! ` + userOption +  ' beats ' + computerResult;
+  } else if (computerResult === userOption) {
+    return `It's a tie! Both chose ` + userOption;
   } else {
     computerScore++;
-    result =  'You Lose Computer Wins'
-    
+    return "Computer wins! " + computerResult + ' beats ' + userOption;
+  }
+};
+
+
+function showResults(userOption) {
+
+  const div = document.createElement('div');
+  div.textContent = getRoundResults(userOption);
+  document.body.appendChild(div);
+
+  const scoreDiv = document.createElement('div');
+    scoreDiv.textContent = 'human ' + humanScore + " | computer " + computerScore;
+    document.body.appendChild(scoreDiv);
+
+
+  if (humanScore === 3 || computerScore === 3) {
+    scoreDiv.textContent =`${
+      humanScore === 3 ? "Player" : "Computer"
+    } has won the game!`;
+
   }
 
-    const div = document.createElement('div');
-    div.textContent = result;
+};
 
-    const scoreDiv = document.createElement('div');
-    scoreDiv.textContent = 'human' + humanScore + " | computer " + computerScore;
-
-   
-
-  }
   
-
-}
-
-
-playGame();
-
-
-
 const rock = document.createElement('button');
 rock.textContent = 'rock';
+document.body.appendChild(rock);
+
+rock.addEventListener('click',function(){
+  showResults('rock');
+})
+
 const paper = document.createElement('button');
 paper.textContent = 'paper';
+document.body.appendChild(paper);
+
+paper.addEventListener('click',function(){
+  showResults('paper');
+})
+
 const scissors = document.createElement('button');
-scissors.textContent = 'scissor';
+scissors.textContent = 'scissors';
+document.body.appendChild(scissors);
+
+scissors.addEventListener('click',function(){
+  showResults('scissors');
+});
 
 
 
